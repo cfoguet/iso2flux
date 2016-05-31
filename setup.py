56b8825e2abd108c319d7b79ebe1f26872327969
cfoguet <cfoguet@outlook.com>
2016-05-31 10:24:08 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+
+from setuptools import setup, find_packages
+
+setup(name='iso2flux',
+      version='0.1',
+      description='iso2flux',
+      author='Carles Foguet',
+      author_email='cfoguet@outlook.com',
+      url='',
+      packages=find_packages(),
+     )
