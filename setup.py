4df88cb186b3114160ea9412dc5c077e573825f9
cfoguet <cfoguet@outlook.com>
2016-07-20 18:02:58 +0200

    Fixed Setup.pt

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@
 from setuptools import setup, find_packages
 
 setup(name='iso2flux',
-      version='0.1',
+      version='0.2',
       description='iso2flux',
       author='Carles Foguet',
       author_email='cfoguet@outlook.com',
       url='',
-      scripts=["run_is2oflux.py"],
+      scripts=["run_is2oflux_cl.py","run_is2oflux_gui.py"],
       install_requires=["cython","python-libsbml","openpyxl","numpy","scipy","matplotlib","pandas","nose","lxml","cobra"],
       packages=find_packages(),
      )
