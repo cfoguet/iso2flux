0c69bc1357ba9b6680b09870e9e45cca9c667885
cfoguet <cfoguet@outlook.com>
2016-11-08 13:36:29 +0100

    Minor fixes in the validate option

--- iso2flux/gui/gui.py
+++ iso2flux/gui/gui.py
@@ -2381,7 +2381,8 @@ class build_model_gui:
           emu_dict0,label_model.experimental_dict =read_experimental_mid(label_model,e_data_names,emu0_dict={},experimental_dict={},minimum_sd=p_dict["minimum_sd"])
           label_model.build(emu_dict0,force_balance=True,recompile_c_code=True,remove_impossible_emus=True,isotopic_steady_state=True,excluded_outputs_inputs=[],turnover_upper_bound=p_dict["turnover_upper_bound"],clear_intermediate_data=True,turnover_exclude_EX=p_dict['turnover_exclude_EX'])
           self.finish_create_new_model() 
-        except:
+        except Exception, e:
+           print e 
            top=Toplevel()
            input_missing_label=Label(top,text="Warning:\nIso2flux was unable to build the label propagation\nmodel due to missing or wrong inputs.\nThis will prevent simulating 13C propagation.\n Do you wish to continue?")
            input_missing_label.pack(side=TOP)
@@ -2398,11 +2399,16 @@ class build_model_gui:
         self.root.destroy()  
         
     def validate_model(self):
+        e=None
+        steady_state_flag=False
+        missing_dict={}
         self.root.withdraw() 
         self.store_inputs()
         if ""==self.sbml_entry.get() or ""==self.e_data_entry.get or ""==self.label_rules_entry.get():
              print "Mandatory Input missing"
              return
+        with open("validation_results.txt","w") as f:
+                 f.write("Validation summary:\n") 
         p_dict={'reactions_with_forced_turnover': [], 'annealing_cycle_time_limit': 1800, 'confidence_max_absolute_perturbation': 10, 'turnover_exclude_EX': True, 'annealing_n_processes': 4, 'annealing_p0': 0.4, 'identify_free_parameters_add_turnover': True, 'minimum_sd': 0.01, 'annealing_max_perturbation': 1, 'turnover_upper_bound': 100, 'confidence_perturbation': 0.1, 'annealing_m': 1000, 'annealing_n': 10, 'annealing_relative_max_sample': 0.35, 'confidence_min_absolute_perturbation': 0.05, 'annealing_pf': 0.0001, 'confidence_significance': 0.95, 'identify_free_parameters_change_threshold': 0.005, 'parameter_precision': 0.0001, 'fraction_of_optimum': 1.0, 'lp_tolerance_feasibility': 1e-09, 'identify_free_parameters_n_samples': 200, 'annealing_relative_min_sample': 0.2, 'annealing_iterations': 2}
         model_file=self.sbml_entry.get()
         if ".sbml" in model_file.lower() or ".xml" in model_file.lower():
@@ -2418,28 +2424,41 @@ class build_model_gui:
           p_dict.update(read_isotoflux_settings(settings_entry.get()))
           print "Settings loaded"
         except:
-          pass 
-        self.label_model=label_model=Label_model(model,lp_tolerance_feasibility=p_dict["lp_tolerance_feasibility"],parameter_precision=p_dict["parameter_precision"],reactions_with_forced_turnover=p_dict["reactions_with_forced_turnover"])
-        self.label_model.project_name="validation"
-        read_isotopomer_model(label_model,self.label_rules_entry.get())
-        missing_dict=find_missing_reactions(label_model,fn="validation_results.txt",fn_mode="w")
-        #print self.label_model.flux_dict
-        if len(missing_dict)==0:
-           #loaded_file = tkFileDialog.askopenfile(title='Choose experimental measuments file',filetypes=[("xlsx",".xlsx")]) 
-           #fileName=loaded_file.name
-           e_data_names=self.e_data_entry.get().replace("[","").replace("]","").split(",")
-           emu_dict0,label_model.experimental_dict =read_experimental_mid(label_model,e_data_names,emu0_dict={},experimental_dict={},minimum_sd=p_dict["minimum_sd"])
-           label_model.build(emu_dict0,force_balance=False,recompile_c_code=True,remove_impossible_emus=True,isotopic_steady_state=True,excluded_outputs_inputs=[],turnover_upper_bound=p_dict["turnover_upper_bound"],clear_intermediate_data=False,turnover_exclude_EX=p_dict['turnover_exclude_EX'])
-           steady_state_flag=check_steady_state(label_model,only_initial_m0=True,threshold=1e-9,fn="validation_results.txt",fn_mode="a")#Check initial dy for steady state deviations
-            #check_simulated_fractions(label_model)
-            #self.label_model.p_dict=p_dict
-            #save_iso2flux_model(label_model,name="project",write_sbml=True,gui=True)
-            #self.root.destroy()
-        else:
-           steady_state_flag=False    
+          pass
+        try:
+          if model.optimize().status=="infeasible":
+             feasible=False
+             with open("validation_results.txt","a") as f:
+                 f.write("Error:Constraint based model is infeasible with the current constraints"+"\n") 
+          else:
+             feasible=True    
+             self.label_model=label_model=Label_model(model,lp_tolerance_feasibility=p_dict["lp_tolerance_feasibility"],parameter_precision=p_dict["parameter_precision"],reactions_with_forced_turnover=p_dict["reactions_with_forced_turnover"])
+             self.label_model.project_name="validation"
+             read_isotopomer_model(label_model,self.label_rules_entry.get())
+             missing_dict=find_missing_reactions(label_model,fn="validation_results.txt",fn_mode="a")
+             #print self.label_model.flux_dict
+             if len(missing_dict)==0:
+              #loaded_file = tkFileDialog.askopenfile(title='Choose experimental measuments file',filetypes=[("xlsx",".xlsx")]) 
+              #fileName=loaded_file.name
+              e_data_names=self.e_data_entry.get().replace("[","").replace("]","").split(",")
+              emu_dict0,label_model.experimental_dict =read_experimental_mid(label_model,e_data_names,emu0_dict={},experimental_dict={},minimum_sd=p_dict["minimum_sd"])
+              label_model.build(emu_dict0,force_balance=False,recompile_c_code=True,remove_impossible_emus=True,isotopic_steady_state=True,excluded_outputs_inputs=[],turnover_upper_bound=p_dict["turnover_upper_bound"],clear_intermediate_data=False,turnover_exclude_EX=p_dict['turnover_exclude_EX'])
+              steady_state_flag=check_steady_state(label_model,only_initial_m0=True,threshold=1e-9,fn="validation_results.txt",fn_mode="a")#Check initial dy for steady state deviations
+               
+              #check_simulated_fractions(label_model)
+              #self.label_model.p_dict=p_dict
+              #save_iso2flux_model(label_model,name="project",write_sbml=True,gui=True)
+              #self.root.destroy()
+             else:
+               steady_state_flag=False
+        except Exception, e:
+            print e
+            with open("validation_results.txt","a") as f:
+                 f.write(str(e)+"\n")       
+        
         top=Toplevel()
         top.protocol("WM_DELETE_WINDOW",self.restart_script)
-        if steady_state_flag and len(missing_dict)==0:
+        if steady_state_flag and len(missing_dict)==0 and e==None and feasible==True:
            validation_summary=Label(top,text="Succesfully validated\n")
         else:  
            validation_summary=Label(top,text="Validation failed,\nsee validation_results.txt for details\n")
