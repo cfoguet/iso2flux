a4b4035533a8a8033ee6225b3b25a5481766992f
Pablo Moreno <pablo.a.moreno@gmail.com>
2017-05-05 14:11:24 +0100

    Sets cobra version for master branch

--- setup.py
+++ setup.py
@@ -10,6 +10,6 @@ setup(name='iso2flux',
       author_email='cfoguet@outlook.com',
       url='',
       scripts=["run_iso2flux_cl.py","run_iso2flux_gui.py"],
-      install_requires=["cython","python-libsbml","openpyxl","numpy","scipy","pandas","nose","lxml","cobra"],
+      install_requires=["cython","python-libsbml","openpyxl","numpy","scipy","pandas","nose","lxml","cobra==0.5.11"],
       packages=find_packages(),
      )
