951604c5cecfab03377bbac70cfe215124543a44
cfoguet <cfoguet@users.noreply.github.com>
2017-12-05 13:15:37 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(name='iso2flux',
       author='Carles Foguet',
       author_email='cfoguet@ub.edu',
       url='',
-      scripts=["create_iso2flux_model.py","solve_iso2flux_label.py","create_and_solve_iso2flux_model.py"],
+      scripts=["create_and_solve_iso2flux_model.py"],
       install_requires=["cloudpickle==0.4","pygmo==2.3","cython","python-libsbml","openpyxl","numpy","scipy","lxml","cobra==0.7"],
       packages=find_packages(),
      )
