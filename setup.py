2370fac14e92da1370d952aa2618e872690e359b
Carles <cfoguet@outlook.com>
2017-12-05 11:16:00 +0100

    Minor fix to setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(name='iso2flux',
       author='Carles Foguet',
       author_email='cfoguet@ub.edu',
       url='',
-      scripts=["create_iso2flux_model.py","get_intervals.py","integrate_gene_expression.py","p13cmfa.py","solve_iso2flux_label.py"],
+      scripts=["create_iso2flux_model.py","get_intervals.py","integrate_gene_expression.py","solve_iso2flux_label.py","create_and_solve_iso2flux_model.py"],
       install_requires=["pygmo","cython","python-libsbml","openpyxl","numpy","scipy","lxml","ipyparallel","cobra==0.7"],
       packages=find_packages(),
      )
